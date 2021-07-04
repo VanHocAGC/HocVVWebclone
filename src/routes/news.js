172d565425c3e1ab5033f26a4660a1b937e4ed64
vanhocAGC <vovanhoc12@gmail.com>
2021-07-04 10:48:10 +0700

    create navbar

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const newsController = require('../app/controllers/NewsController')
 
-router.use('/', newsController.index)
+router.get('/', newsController.index)
 
 
 module.exports = router
