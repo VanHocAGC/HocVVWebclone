c1535db44740f808ff2df09e11689a4868a5ad7e
vanhocAGC <vovanhoc12@gmail.com>
2021-07-14 08:29:53 +0700

    apply scrollspy, AOS and slick

---
+++ src/public/css/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#autoplay').slick({
+        dots: true,
+        dotsClass: 'dot-class',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000 ,
+        arrows:false,
+    });
+});
+
