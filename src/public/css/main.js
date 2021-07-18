282622b78e684a6d1c2915a8a360cc0e786a7c91
vanhocAGC <vovanhoc12@gmail.com>
2021-07-18 13:55:58 +0700

    fix bug

--- src/public/css/main.js
+++ src/public/css/main.js
@@ -1,3 +1,6 @@
+$(window).on('load', function(event) {
+    $('.load').delay(1000).fadeOut('slow');
+})
 $(document).ready(function () {
     $('#autoplay').slick({
         dots: true,
@@ -8,5 +11,23 @@ $(document).ready(function () {
         autoplaySpeed: 2000 ,
         arrows:false,
     });
+    $('.filter .nav-item a').click(function(){
+        var _type = $(this).text()
+        $('.filter .nav-item a').each(function(){
+            if($(this).text()!=_type){
+                $(this).removeClass('active')
+            }else{
+                $(this).addClass('active')
+            }
+        })
+        $('.list-image .image-item').each(function(){
+            if($(this).hasClass(_type)){
+                $(this).removeClass('hidden')
+            }else{
+                $(this).addClass('hidden')
+            }
+
+        })
+    })
 });
 
